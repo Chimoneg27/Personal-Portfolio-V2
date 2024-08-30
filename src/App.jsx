988a662f63680cb61cb11e9b1df2e3461d40bab0
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-08-30 21:02:03 +0200

    add markup part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
+import reactLogo from './assets/react-logo.png';
 
 function App() {
 
   return (
-    <>
-    <h1>Hello World!</h1>
-    </>
+    <div>
+        <img src={reactLogo} width="40px" />
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
   )
 }
 
