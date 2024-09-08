b4ef1986fd2d08780fc2729fc076ff001a4a69a5
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-09-08 23:10:11 +0200

    Add readme message

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import './App.css'
-import reactLogo from './assets/react-logo.png';
 
 function App() {
 
   return (
     <div>
-        <img src={reactLogo} width="40px" />
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
+        <p>Hello React</p>
     </div>
   )
 }
