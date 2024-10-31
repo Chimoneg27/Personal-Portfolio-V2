2b8549e817cb9acd44f2004b0ecc6f99388a98fa
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-10-31 21:26:38 +0200

    add movement logic

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { scaleFactor } from "./constants";
 import { k } from "./kaboomCtx";
+import { displayDialogue } from "./utils";
 
-k.loadSprite("spritesheet", "./spritesheet.png"), {
+k.loadSprite("spritesheet", "./spritesheet.png", {
     sliceX: 39,
     sliceY: 31,
     anims: {
@@ -10,25 +11,25 @@ k.loadSprite("spritesheet", "./spritesheet.png"), {
         "idle-side": 975,
         "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
         "idle-up": 1014,
-        "walk-up": { from: 1014, to:1017, loop: true, speed: 8 }
+        "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 }
     }
-}
+});
 
-k.loadSprite("map", "./map.png")
-k.setBackground(k.Color.fromHex("#311047"))
+k.loadSprite("map", "./map.png");
+k.setBackground(k.Color.fromHex("#311047"));
 
-k.scene("main", async ()=> {
-    const mapData = await (await fetch("./map.json")).json()
-    const layers = mapData.layers
+k.scene("main", async () => {
+    const mapData = await (await fetch("./map.json")).json();
+    const layers = mapData.layers;
 
-    const map = k.make([
+    const map = k.add([
         k.sprite("map"),
         k.pos(0),
         k.scale(scaleFactor)
-    ])
+    ]);
 
     const player = k.make([
-        k.sprite("spritesheet", {anim: "idle-down"}),
+        k.sprite("spritesheet", { anim: "idle-down" }),
         k.area({
             shape: new k.Rect(k.vec2(0, 3), 10, 10),
         }),
@@ -38,15 +39,15 @@ k.scene("main", async ()=> {
         k.scale(scaleFactor),
         {
             speed: 250,
-            direction: down,
+            direction: "down", // Assuming "down" is a string representing direction
             isInDialogue: false,
         },
         "player",
     ]);
 
     for (const layer of layers) {
-        if(layer.name === "boundaries") {
-            for(const boundary of layer.objects) {
+        if (layer.name === "boundaries") {
+            for (const boundary of layer.objects) {
                 map.add([
                     k.area({
                         shape: new k.Rect(k.vec2(0), boundary.width, boundary.height)
@@ -54,17 +55,43 @@ k.scene("main", async ()=> {
                     k.body({ isStatic: true }),
                     k.pos(boundary.x, boundary.y),
                     boundary.name,
-                ])
+                ]);
 
-                if(boundary.name) {
-                    player.onCollide(boundary.name, ()=> {
+                if (boundary.name) {
+                    player.onCollide(boundary.name, () => {
                         player.isInDialogue = true;
-                        //TODO
-                    })
+                        displayDialogue("test", () => (player.isInDialogue = false));
+                    });
+                }
+            }
+            continue;
+        }
+
+        if (layer.name === "spawnpoints") {
+            for (const entity of layer.objects) {
+                if (entity.name === "player") {
+                    player.pos = k.vec2(
+                        (map.pos.x + entity.x) * scaleFactor,
+                        (map.pos.y + entity.y) * scaleFactor
+                    );
+
+                    k.add(player);
+                    continue;
                 }
             }
         }
     }
-})
 
-k.go("main")
+    k.onUpdate(() => {
+        k.camPos(player.pos.x, player.pos.y + 100);
+    });
+
+    k.onMouseDown((mouseBtn) => {
+        if (mouseBtn !== "left" || player.isInDialogue) return;
+
+        const worldMousePos = k.toWorld(k.mousePos());
+        player.moveTo(worldMousePos, player.speed)
+    })
+});
+
+k.go("main");
