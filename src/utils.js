4ad04ea4e47bd55d7b80099a8d2b041674c309b1
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-10-31 21:36:27 +0200

    add scale logic for different screen sizes

--- src/utils.js
+++ src/utils.js
@@ -30,3 +30,13 @@ export const displayDialogue =(text, onDisplayEnd) => {
 
     closeBtn.addEventListener("click", onCloseButtonClick)
 }
+
+export const setCamScale = (k) => {
+    const resizeFactor = k.width() / k.height()
+    if(resizeFactor < 1) {
+        k.camScale(k.vec2(1))
+        return;
+    }
+
+    k.camScale(k.vec2(1.5))
+}
