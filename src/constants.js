8ded892b56b48002436a0487b0dfeb4017c52d1b
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-03 22:26:48 +0200

    add dialogue for items

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,17 @@
 export const scaleFactor = 4
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript. I am learning python to add to my skills.
+    Here is my <a href="https://github.com/Chimoneg27" target="_blank">Github</a>!`,
+  "cs-degree": `This is my Microverse Certificate. I hung it on the wall because I'm proud of it. See it <a href="https://github.com/Chimoneg27/Garvin-Chimone/blob/main/mvfsc.pdf" target="_blank">here</a>`,
+  "sofa-table": `Here is the sofa. I watch tv from here and the occasional game. I have a PS5 slim`,
+  tv: `That's my TV. I've been watching The Boys and The Penguin (show of the year)`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/Chimoneg27/Garvin-Chimone/blob/main/GCresumeneww.pdf" target="_blank">Check it out?</a>
+  Contact me at chimonegarvin27@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of books on my shelves. There are some Spanish one (I am learning to speak Spanish btw).
+  I probably only read one of them. Who else compulsively buys books without ever finishing them?`,
+  exit: `If you want to exit portfolio, just close the tab.`
+}
